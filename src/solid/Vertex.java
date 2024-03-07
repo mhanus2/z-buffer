074d2ef2e103c87274438ead71f4d20bebfaaa25
mhanus2 <mhmartinhanus@gmail.com>
2024-03-07 21:31:43 +0100

    Added: Vertex uv, mul, add

--- src/solid/Vertex.java
+++ src/solid/Vertex.java
@@ -2,16 +2,19 @@ package solid;
 
 import transforms.Col;
 import transforms.Point3D;
+import transforms.Vec2D;
 
-public class Vertex {
+public class Vertex implements Vectorizable<Vertex> {
     private final Point3D position;
     private final Col color;
+    private final Vec2D uv;
 
     // TODO: souřadnice do textury, normála atd.
 
-    public Vertex(Point3D position, Col color) {
+    public Vertex(Point3D position, Col color, Vec2D uv) {
         this.position = position;
         this.color = color;
+        this.uv = uv;
     }
 
     public Point3D getPosition() {
@@ -20,5 +23,23 @@ public class Vertex {
 
     public Col getColor() {
         return color;
+    }
+
+    @Override
+    public Vertex mul(double k) {
+        return new Vertex(position.mul(k), color.mul(k), uv.mul(k));
+    }
+
+    @Override
+    public Vertex add(Vertex v) {
+        return new Vertex(
+                position.add(v.getPosition()),
+                color.add(v.getColor()), uv.add(v.getUv())
+        );
+
+    }
+
+    private Vec2D getUv() {
+        return uv;
     }
 }
