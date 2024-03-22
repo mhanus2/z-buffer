a5f44141af4faad852efd7a495ac96dad84aeaf2
mhanus2 <mhmartinhanus@gmail.com>
2024-03-22 23:25:47 +0100

    Added: Constructor, dehomog, getUv

--- src/solid/Vertex.java
+++ src/solid/Vertex.java
@@ -3,13 +3,22 @@ package solid;
 import transforms.Col;
 import transforms.Point3D;
 import transforms.Vec2D;
+import transforms.Vec3D;
+
+import java.util.Optional;
 
 public class Vertex implements Vectorizable<Vertex> {
     private final Point3D position;
     private final Col color;
     private final Vec2D uv;
 
-    // TODO: souřadnice do textury, normála atd.
+    // TODO: souřadnice do textury atd.
+
+    public Vertex(Point3D position, Col color) {
+        this.position = position;
+        this.color = color;
+        this.uv = new Vec2D();
+    }
 
     public Vertex(Point3D position, Col color, Vec2D uv) {
         this.position = position;
@@ -24,6 +33,9 @@ public class Vertex implements Vectorizable<Vertex> {
     public Col getColor() {
         return color;
     }
+    public Vec2D getUv() {
+        return uv;
+    }
 
     @Override
     public Vertex mul(double k) {
@@ -34,12 +46,16 @@ public class Vertex implements Vectorizable<Vertex> {
     public Vertex add(Vertex v) {
         return new Vertex(
                 position.add(v.getPosition()),
-                color.add(v.getColor()), uv.add(v.getUv())
+                color.add(v.getColor()),
+                uv.add(v.getUv())
         );
 
     }
 
-    private Vec2D getUv() {
-        return uv;
+    public Optional<Vertex> dehomog() {
+        Optional<Vec3D> optional = position.dehomog();
+        return optional.map(vec3D -> new Vertex(new Point3D(vec3D), color, uv));
     }
+
+
 }
