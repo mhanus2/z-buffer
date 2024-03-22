2a663195869f155fe89ca4205add0e1191dd809d
mhanus2 <mhmartinhanus@gmail.com>
2024-03-22 23:25:15 +0100

    Added: Model mat

--- src/solid/Solid.java
+++ src/solid/Solid.java
@@ -1,11 +1,15 @@
 package solid;
 
+import transforms.Mat4;
+import transforms.Mat4Identity;
+
 import java.util.ArrayList;
 
 public abstract class Solid {
     protected  final ArrayList<Part> partBuffer = new ArrayList<>();
     protected  final ArrayList<Vertex> vertexBuffer = new ArrayList<>();
     protected  final ArrayList<Integer> indexBuffer = new ArrayList<>();
+    private Mat4 model = new Mat4Identity();
 
     public ArrayList<Part> getPartBuffer() {
         return partBuffer;
@@ -17,5 +21,11 @@ public abstract class Solid {
 
     public ArrayList<Integer> getIndexBuffer() {
         return indexBuffer;
+    }
+    public Mat4 getModel() {
+        return model;
+    }
+    public void setModel(Mat4 model) {
+        this.model = model;
     }
 }
