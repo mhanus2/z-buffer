5ef18affd2ed03476aa5cfd80b9a9a657d405ed1
mhanus2 <mhmartinhanus@gmail.com>
2024-03-22 23:27:29 +0100

    Added: Camera, projection

--- src/control/Controller3D.java
+++ src/control/Controller3D.java
@@ -4,17 +4,19 @@ import raster.Raster;
 import raster.TriangleRasterizer;
 import raster.ZBuffer;
 import solid.Vertex;
-import transforms.Col;
-import transforms.Point3D;
+import transforms.*;
 import view.Panel;
 
 import java.awt.event.*;
 
 public class Controller3D implements Controller {
     private final Panel panel;
-
     private ZBuffer zBuffer;
     private TriangleRasterizer triangleRasterizer;
+    private Camera camera;
+    private Mat4 proj;
+    private double azimuth = 90;
+    private double zenith = -15;
 
     public Controller3D(Panel panel) {
         this.panel = panel;
@@ -28,6 +30,21 @@ public class Controller3D implements Controller {
 
         zBuffer = new ZBuffer(raster);
         triangleRasterizer = new TriangleRasterizer(zBuffer);
+
+        camera = new Camera(
+                new Vec3D(1, -5, 2),
+                Math.toRadians(azimuth),
+                Math.toRadians(zenith),
+                1,
+                true
+        );
+
+        proj = new Mat4PerspRH(
+                Math.PI / 3,
+                raster.getHeight() / (double) raster.getWidth(),
+                0.5,
+                50
+        );
     }
 
     @Override
@@ -48,9 +65,9 @@ public class Controller3D implements Controller {
 //        zBuffer.setPixelWithZTest(10, 10, 0.7, new Col(0x00ff00));
 
         triangleRasterizer.rasterize(
-                new Vertex(new Point3D(400, 0, 0.5), new Col(0xff0000)),
-                new Vertex(new Point3D(0, 300, 0.5), new Col(0x00ff00)),
-                new Vertex(new Point3D(799, 599, 0.5), new Col(0x0000ff))
+                new Vertex(new Point3D(400, 0, 0.5), new Col(0xff0000), new Vec2D(0)),
+                new Vertex(new Point3D(0, 300, 0.5), new Col(0x00ff00), new Vec2D(0)),
+                new Vertex(new Point3D(799, 599, 0.5), new Col(0x0000ff), new Vec2D(0))
         );
 
         panel.repaint();
