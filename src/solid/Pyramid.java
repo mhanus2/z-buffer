e34198351b119edc0007ba5738a415bd0d1be491
mhanus2 <mhmartinhanus@gmail.com>
2024-04-20 16:10:46 +0200

    Added: Pyramid

---
+++ src/solid/Pyramid.java
@@ -0,0 +1,37 @@
+package solid;
+
+import transforms.Col;
+import transforms.Point3D;
+
+
+public class Pyramid extends Solid {
+    public Pyramid() {
+        Vertex A = new Vertex(new Point3D(0,0,0), new Col(255,0,0));
+        Vertex B = new Vertex(new Point3D(1,0,0), new Col(0,255,0));
+        Vertex C = new Vertex(new Point3D(0.5,1,0), new Col(0,0,255));
+        Vertex D = new Vertex(new Point3D(0.5,0.5,1), new Col(255,255,255));
+
+        vertexBuffer.add(A);
+        vertexBuffer.add(B);
+        vertexBuffer.add(C);
+        vertexBuffer.add(D);
+
+        indexBuffer.add(0);
+        indexBuffer.add(1);
+        indexBuffer.add(2);
+
+        indexBuffer.add(0);
+        indexBuffer.add(1);
+        indexBuffer.add(3);
+
+        indexBuffer.add(0);
+        indexBuffer.add(2);
+        indexBuffer.add(3);
+
+        indexBuffer.add(1);
+        indexBuffer.add(2);
+        indexBuffer.add(3);
+
+        partBuffer.add(new Part(TopologyType.TRIANGLES,0,4));
+    }
+}
