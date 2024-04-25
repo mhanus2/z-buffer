be79ad330a84632affd8fccc7fa600b23245ce33
mhanus2 <mhmartinhanus@gmail.com>
2024-04-25 20:23:19 +0200

    Added: LineRasterizer

---
+++ src/raster/LineRasterizer.java
@@ -0,0 +1,75 @@
+package raster;
+
+import solid.Vertex;
+import transforms.Point3D;
+import transforms.Vec3D;
+
+import java.util.Optional;
+
+public class LineRasterizer extends Rasterizer{
+
+    public LineRasterizer(ZBuffer zBuffer) {
+        super(zBuffer);
+    }
+
+    @Override
+    public void rasterize(Vertex a, Vertex b, Vertex c) {
+        rasterize(a, b);
+        rasterize(b, c);
+        rasterize(a, c);
+    }
+
+    public void rasterize(Vertex a, Vertex b) {
+        // Dehomogenization
+        Optional<Vertex> newA = a.dehomog();
+        Optional<Vertex> newB = b.dehomog();
+
+        if (newA.isEmpty() || newB.isEmpty()) return;
+
+        Vertex nA = newA.get();
+        Vertex nB = newB.get();
+
+        // Transformation to window
+        Vec3D vec3D1 = transformToWindow(nA.getPosition());
+        a = new Vertex(new Point3D(vec3D1), nA.getColor());
+        Vec3D vec3D2 = transformToWindow(nB.getPosition());
+        b = new Vertex(new Point3D(vec3D2), nB.getColor());
+
+        // Sorting y
+        if (a.getPosition().getY() > b.getPosition().getY()) {
+            Vertex temp = a;
+            a = b;
+            b = temp;
+        }
+
+        double dxAB = a.getPosition().getX() - b.getPosition().getX();
+        double dyAB = a.getPosition().getY() - b.getPosition().getY();
+        double dzAB = a.getPosition().getZ() - b.getPosition().getZ();
+
+        // Určíme, která osa je hlavní (ta s největším rozdílem)
+        double steps = Math.max(Math.abs(dxAB), Math.abs(dyAB));
+        steps = Math.max(steps, Math.abs(dzAB));
+
+        // Vypočítáme přírůstky pro každou osu
+        double xIncrement = dxAB / steps;
+        double yIncrement = dyAB / steps;
+        double zIncrement = dzAB / steps;
+
+        // Inicializujeme proměnné pro průchod přímkou
+        double x = a.getPosition().getX(), y = a.getPosition().getY(), z = a.getPosition().getZ();
+
+        // Projdeme přímkou a vykreslíme pixely, pokud jsou blíže než stávající pixel v z-bufferu
+        for (int i = 0; i <= steps; i++) {
+            int xi = (int) Math.round(x);
+            int yi = (int) Math.round(y);
+
+            // Zkontrolujeme z-buffer
+            zBuffer.setPixelWithZTest(xi, yi, z, a.getColor());
+
+            // Posuneme se na další bod na přímce
+            x -= xIncrement;
+            y -= yIncrement;
+            z += zIncrement;
+        }
+    }
+}
